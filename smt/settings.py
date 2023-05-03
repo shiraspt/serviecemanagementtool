f3ab3ce11c56089597673de3970f5ee37d22cfca
shiras <pshiras455@gmail.com>
2023-05-03 13:14:58 +0530

    first commit

--- smt/settings.py
+++ smt/settings.py
@@ -134,10 +134,10 @@ staticfiles_DIRS=(os.path.join(BASE_DIR,'static'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = ''
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = 'onoxtfcmtwqcrlhi'
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 
